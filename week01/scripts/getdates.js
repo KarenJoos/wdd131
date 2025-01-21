4f03546083f2900b216b5804335860f71cd99da9
KarenJoos <karen.karebear@gmail.com>
2025-01-20 17:22:56 -0700

    Javascript updates

--- week01/scripts/getdates.js
+++ week01/scripts/getdates.js
@@ -1,2 +1,6 @@
-let 
-(document.lastModified);
+let text = document.lastModified;
+document.getElementById("lastModified").innerHTML = text;
+
+const d = new Date();
+let currentyear = d.getFullYear();
+document.getElementById('currentyear').innerHTML = currentyear;
