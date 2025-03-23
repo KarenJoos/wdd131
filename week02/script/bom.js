191d2cac0dc7ba6a18c24c6fd16205a708491428
KarenJoos <karen.karebear@gmail.com>
2025-03-22 21:16:06 -0600

    bom updates

--- week02/script/bom.js
+++ week02/script/bom.js
@@ -1,39 +1,30 @@
 const input = document.querySelector('favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('bom.html');
+const list = document.querySelector('______');
 
-const li = document.createElement('li');
 
-const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
-
-deleteButton.textContent = "❌";
-
-li.append(deleteButton);
-
-list.append(li)
-
-addbutton.addEventListener('click', function () {
-    
-    if (input.value.trim() !== '') { }
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
         const li = document.createElement('li');
 
-        li.textContent = input.value;
-        input.value = '';
-        
-    
         const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+
         deleteButton.textContent = "❌";
-    
         deleteButton.addEventListener('click', function () {
             list.removeChild(li);
             input.focus();
-        });
+            input.value = '';
+        })
 
         li.append(deleteButton);
 
-        list.append(li)
-});
+        list.append(li);
+
+        input.focus();
+    }
+})
 
 
