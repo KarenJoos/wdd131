2b32ff16c4386c294e107592215427ffab66e491
KarenJoos <karen.karebear@gmail.com>
2025-04-12 15:12:38 -0600

    updates

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,154 @@
+const hambutton = document.querySelector('#menu');
+const mainnav = document.querySelector('.navigation');
+const hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', () => {
+    mainnav.classList.toggle('active');
+    hamburger.classList.toggle('active');
+})
+
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    mainnav.classList.remove('active');
+}))
+
+const temples = [
+    {
+        templeName: "Aba Nigeria",
+        location: "Aba, Nigeria",
+        dedicated: "2005, August, 7",
+        area: 11500,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    },
+    {
+        templeName: "Manti Utah",
+        location: "Manti, Utah, United States",
+        dedicated: "1888, May, 21",
+        area: 74792,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    },
+    {
+        templeName: "Payson Utah",
+        location: "Payson, Utah, United States",
+        dedicated: "2015, June, 7",
+        area: 96630,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    },
+    {
+        templeName: "Yigo Guam",
+        location: "Yigo, Guam",
+        dedicated: "2020, May, 2",
+        area: 6861,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    },
+    {
+        templeName: "Washington D.C.",
+        location: "Kensington, Maryland, United States",
+        dedicated: "1974, November, 19",
+        area: 156558,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    },
+    {
+        templeName: "Lima Perú",
+        location: "Lima, Perú",
+        dedicated: "1986, January, 10",
+        area: 9600,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    },
+    {
+        templeName: "Mexico City Mexico",
+        location: "Mexico City, Mexico",
+        dedicated: "1983, December, 2",
+        area: 116642,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    },
+    {
+        templeName: "San Diego California",
+        location: "San Diego, California, United States",
+        dedicated: "1993, April, 25",
+        area: 72000,
+        imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/san-diego-california-temple/san-diego-california-temple-51787-thumb.jpg"
+    },
+    {
+        templeName: "Salt Lake",
+        location: "Salt Lake City, Utah, United States",
+        dedicated: "1893, April, 6",
+        area: 382207,
+        imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/salt-lake-temple/salt-lake-temple-4937-thumb.jpg"
+    },
+    {
+        templeName: "Idaho Falls",
+        location: "Idaho Falls, Idaho, United States",
+        dedicated: "1945, September, 23",
+        area: 85624,
+        imageURL:
+            "https://churchofjesuschristtemples.org/assets/img/temples/idaho-falls-idaho-temple/idaho-falls-idaho-temple-1903-thumb.jpg"
+    },
+    {
+        templeName: "Provo City Center",
+        location: "Provo, Utah, United States",
+        dedicated: "2016, March, 20",
+        area: 85084,
+        imageURL: "https://churchofjesuschristtemples.org/assets/img/temples/provo-city-center-temple/provo-city-center-temple-3391-thumb.jpg"
+    }
+];
+
+createTempleCard(temples);
+
+const oldLink = document.querySelector("#old");
+const newLink = document.querySelector("#new");
+const largeLink = document.querySelector("#large");
+const smallLink = document.querySelector("#small");
+
+oldLink.addEventListener("click", () => {
+    createTempleCard(temples.filter(temple => temple.dedicated.includes("1800")))
+});
+
+newLink.addEventListener("click", () => {
+    createTempleCard(temples.filter(temple => temple.dedicated.includes("2000")))
+});
+
+largeLink.addEventListener("click", () => {
+    createTempleCard(temples.filter(temple => temple.area.includes(10000)))
+});
+
+smallLink.addEventListener("click", () => {
+    createTempleCard(temples.filter(temple => temple.area.includes(90000)))
+});
+
+function createTempleCard() {
+    document.querySelector(".res-grid").innerHTML = "";
+    filteredtemples.foreach(temple => {
+        let card = document.createElement("section");
+        let name = document.createElement("h3");
+        let location = document.createElement("p");
+        let dedication = document.createElement("p");
+        let area = document.createElement("p");
+        let img = document.createElement("img");
+        
+        name.textContent = temple.templeName;
+        location.innerHTML = '<span class="label">Location:</span> ${temple.location}';
+        dedication.innerHTML = '<span class="label">Dedicated:</span> ${temple.dedicated}';
+        area.innerHTML = '<span class="label">Size:</span> ${temple.area} sq ft';
+        img.setAttribute("src", temple.imageUrl);
+        img.setAttribute("alt", '${temple.templeName} Temple');
+        img.setAttribute("loading", "lazy");
+
+        card.appendChild(name);
+        card.appendChild(locattion);
+        card.appendChild(dedication);
+        card.appendChild(area);
+        card.appendChild(img);
+    });
+
+    document.querySelector(".res-grid").appendChild(card);
+}
