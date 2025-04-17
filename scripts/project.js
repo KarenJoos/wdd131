964b5627919cd2dad8d0716b341284d5b69664ac
KarenJoos <karen.karebear@gmail.com>
2025-04-17 07:42:35 -0600

    project updates

--- scripts/project.js
+++ scripts/project.js
@@ -5,3 +5,16 @@ const d = new Date();
 let currentyear = d.getFullYear();
 document.getElementById('currentyear').innerHTML = currentyear;
 
+const hambutton = document.querySelector('#menu');
+const mainnav = document.querySelector('.navigation');
+const hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', () => {
+    mainnav.classList.toggle('active');
+    hamburger.classList.toggle('active');
+})
+
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    mainnav.classList.remove('active');
+}))
