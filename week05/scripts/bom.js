a8055c8c688f543cd032b34471b356d056dfd88b
KarenJoos <karen.karebear@gmail.com>
2025-04-16 18:23:04 -0600

    updates

---
+++ week05/scripts/bom.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const input = document.querySelector('#favchap');
+    const button = document.querySelector('#submit');
+    const list = document.querySelector('#list');
+
+    let chaptersArray = new Array();
+    let 
+
+
+
+    button.addEventListener('click', () => {
+        if (input.value.trim() !== '') {
+            const li = document.createElement('li');
+
+            const deleteButton = document.createElement('button');
+
+            li.textContent = input.value;
+
+            deleteButton.textContent = "❌";
+            deleteButton.addEventListener('click', function () {
+                list.removeChild(li);
+                input.focus();
+                input.value = '';
+            })
+
+            li.append(deleteButton);
+
+            list.append(li);
+
+            input.focus();
+        }
+    });
+});
